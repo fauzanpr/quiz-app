4d2754b3279215fc19444e34a01f6dea39a08686
Fauzan Pradana <fauzanpr.06@gmail.com>
2024-08-04 19:36:06 +0700

    feat(quiz): show question

--- src/features/quiz/views/Quiz.tsx
+++ src/features/quiz/views/Quiz.tsx
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
 import Timer from "../../_global/components/Timer"
-import { questionsList } from "../utils/question";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAtom } from "jotai";
+import { questionsListAtom } from "../store/questions";
 
 function Quiz() {
   const { id } = useParams();
+  const [questionsList] = useAtom(questionsListAtom);
+  const navigate = useNavigate();
   useEffect(() => {
     console.log(questionsList);
-  }, []);
+  }, [questionsList]);
+
+  const changeNumHandler = (num: number) => {
+    navigate(`/quiz/${num}`);
+  };
 
   // will check resume quiz
   if (localStorage.getItem("previous-quiz")) {
@@ -20,16 +27,14 @@ function Quiz() {
 
       {/* for displaying number navigation */}
       <div className="grid grid-cols-10 gap-4 mt-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => <button className="bg-gray-200 rounded-lg">{num}</button>)}
-        <button className="bg-green-300 rounded-lg">{11}</button>
-        <button className="bg-white rounded-lg">{12}</button>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => <button className="bg-gray-200 rounded-lg" onClick={() => changeNumHandler(num)}>{num}</button>)}
       </div>
 
       {/* for displaying quiz */}
       <div className="flex flex-col gap-4">
         <div>
           <p className="font-medium underline">Soal {Number(id)}/10</p>
-          <p dangerouslySetInnerHTML={{__html: questionsList[Number(id)]?.question}} className="text-lg" />
+          { questionsList && <p dangerouslySetInnerHTML={{__html: questionsList[Number(id)-1]?.question}} className="text-lg" /> }
         </div>
         <div className="flex items-center gap-2 text-lg">
           <input type="radio" name="ans" className="size-4" />
